08d3895d38dfab3774f0bfefb2ff4ca117ab3159
OleJake <jacobshawn8@outlook.com>
2023-04-23 20:03:58 -0400

    add animations and click event handlers
    add animations and click event handlers

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const buttonColors = ["red", "blue", "green", "yellow"];
 const gamePattern = [];
+const userClickedPattern = [];
 
 function nextSequence() {
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
   gamePattern.push(randomChosenColor);
-  console.log(randomNumber);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  let audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
 }
+
+$(".btn").on("click", function () {
+  let userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  console.log(userChosenColor);
+});
 
 nextSequence();
